0a99a01d47b846047b32023c0a5b4747fc87802c
m <mattialancellotti@protonmail.com>
2019-10-30 17:16:04 +0100

    Added some macros like the version and the colors

--- main.h
+++ main.h
@@ -6,15 +6,25 @@ FLOW:
 
 TODO:
 	+ [ ] Add signal interrupt exception handler
-	+ [ ] Errors/Warnings handler
 	+ [ ] I/O for files
+	+ [ ] Debugger
 */
 #ifndef BRAINFUCK_H
 #define BRAINFUCK_H
 
+#define VERSION 1.1
+#define DATE_OF_RELEASE "2019/10/27"
 #define BUFFER 50
+#define COLORS
 
-enum ARGS_ERRORS   { UNKNWON_ARGS  = 1 };
+#ifdef COLORS
+#define NO_COLORS "\033[0m"
+#define BOLD "\033[1m"
+#define RED "\033[31m"
+#define YELLOW "\033[33m"
+#endif
+
+enum ARGS_ERRORS   { UNKNOWN_ARGS  = 10 };
 enum CODE_WARNINGS { MOVING_OUT_OF_BUFFER = 1 };
 enum CODE_ERRORS   { NO_END_LOOP   	  = 1, NO_START_LOOP = 2, UNKNOWN_KEYWORD = 3 };
 
@@ -29,15 +39,18 @@ struct settings {
 };
 
 void init(int **, struct settings **);
-void analyzer(const char *, int *, int);
+void interpreter(const char *, int *, int);
 struct loop_t *push(unsigned, struct loop_t *, struct loop_t *);
 void xfree_loop(struct loop_t **);
 void xfree_sett(struct settings **);
-void help(void);
-void show_buf(int *);
+void show_buf(const int *);
 int check_errs(const char *);
 int check_warns(const char *);
 struct settings *sett_new(const char *, const char *, int);
-struct settings *handle_args(int argc, const char * const * const argv);
+struct settings *handle_args(const int argc, const char * const * const argv);
+void print_console_warn(const int);
+void print_console_errs(const int);
+void print_console_help(void);
+void print_console_version(void);
 
 #endif
