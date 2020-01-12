2c00160f32f10fb66557a9fc00e421288b78b3d1
m <mattialancellotti@protonmail.com>
2020-01-12 20:05:32 +0100

    Optimized the function for parsing the arguments

--- args.c
+++ args.c
@@ -1,61 +1,41 @@
 #include "args.h"
 #include "mem.h"
 
-struct settings *handle_args(const int argc, const char * const * const argv) {
-	struct settings *new = NULL;
-	const char *code_tmp = NULL;
+struct settings *handle_args(const int argc, char **argv) {
+	char *file_input = NULL;
 	int flags=4, args_errs_flags=0;
 
-	for (int i=1; i<argc; i++)
-		if (argv[i][0] == '-') {
-			if (argv[i][1] == '-') {
-				if (!strcmp(argv[i]+2, "code")) {
-					if (i == argc-1) {
-						args_errs_flags|=MISSING_PARAMETER;
-						goto args_error;
-					} else {
-						flags |= CODE;
-						code_tmp = argv[++i];
-					}
-				} else if (!strcmp(argv[i]+2, "Warnings-on"))
-					flags |= WARNINGS_ON_OFF;
-				else if (!strcmp(argv[i]+2, "Warnings-off"))
-					flags ^= WARNINGS_ON_OFF;
-				else if (!strcmp(argv[i]+2, "help"))
-					flags |= GET_HELP;
-				else if (!strcmp(argv[i]+2, "version"))
-					flags |= GET_VERSION;
-				else {
-					args_errs_flags|=UNKNOWN_ARGS;
-					goto args_error;
-				}
-			} else {
-				switch(argv[i][1]) {
-				case 'c':
-					if (i == argc-1) {
-						args_errs_flags|=MISSING_PARAMETER;
-						goto args_error;
-					} else { 
-						code_tmp = argv[++i];
-						flags |= CODE;
-						break;
-					}
-				case 'h':
-					flags |= GET_HELP;
-					break;
-				case 'v':
-					flags |= GET_VERSION;
-					break;
-				default:
-					args_errs_flags|=UNKNOWN_ARGS;
-					goto args_error;
-				}
-			}
-		} else {
-			args_errs_flags|=UNKNOWN_ARGS;
-			goto args_error;
-		}
+  for (int i=1; i<argc; i++)
+    if (argv[i][0] == '-') {
+     if (strlen(argv[i]) > 1) {
+        switch(argv[i][1]) {
+        case 'i':
+          flags |= GET_INFO;
+          break;
+        case 'h':
+          flags |= GET_HELP;
+          break;
+        case 'v':
+          flags |= GET_VERSION;
+          break;
+        case 's':
+          flags |= GET_STATS;
+          break;
+        default:
+          args_errs_flags = 1;
+          break;
+        }
+     } else
+       printf("Incompleted parameter %s.... Ignoring it.\n", argv[i]);
 
-args_error:
-	return (new = sett_new(code_tmp, flags, args_errs_flags));
+    } else {
+      if (!file_input)
+        file_input = argv[i];
+      else {
+        printf("Multiple input files: %s - %s.", file_input, argv[i]);
+        args_errs_flags = 1;
+      }
+    }
+
+	return sett_new(file_input, flags, args_errs_flags);
 }
--- args.h
+++ args.h
@@ -2,57 +2,44 @@
 #define ARGS_H
 
 #include <string.h>
-
-//ARGS_ERRORS
-#define MISSING_PARAMETER 4
-#define INCOMPATIBLE_ARGS 2
-#define UNKNOWN_ARGS  	  1
+#include <stdio.h>
 
 //ARGS_OPTIONS
-#define CODE              8
-#define WARNINGS_ON_OFF   4
-#define GET_HELP          2
-#define GET_VERSION       1
+#define GET_VERSION 1
+#define GET_HELP    2
+#define GET_INFO    3
+#define GET_STATS   4
 
 struct settings {
-	const char *brainfuck_code;
+  char *finput, *foutput;
 	int flags, args_errs_flags;
 };
 
 //Notes:
-//  [DELETE] Brainfuck code inserted via args
-//      | Not needed since the input will happen via file
-//  [DELETE] ARGS_ERRORS
-//      | There will be a 'Real-Time' errors/warnings processing
 //
-//  [ADDING] Real-Time errors/warnings processing
-//      | To improve speed and to waste less memory in structures
-//  [ADDING] Arguments will have a higher priority then the rest of the program
-//      | Because otherwise you won't be able to make a little mod. to your
-//      | configuration without modifying the config.yaml
-//  [ADDING] -o, --optimize: to optimize the code
-//      | Reducing the amount of instructions
-//  [ADDING] -s, --statistics: outputs some statistics
+//  [ADDING] -i: to output general info
+//      | Combine -h and -v
+//  [ADDING] -s: outputs some statistics
 //      | Just to see how many instructions have been processesed and the time it 
 //      | took
-//  [ADDING] -g, --generate-executable: to generate an executable
+//  [ADDING] -E: to generate an executable
 //      | To execute the code without a compiler/interpreter
+//  [ADDING] New argument format: -o:true, -s:true, -e:false, -fi:<nome>, -fo:<nome>
 //
 //Hypotetical structure for settings
 //
 // struct settings_t {
 //    char *input_f, *output_f;
-//    char *optional_configuration_file; //conf.yaml
 //    //Warnings(on/off), generate an executable, optimization, statistics
 //    int settings_flags;
 // };
 
 /*
- * Handle the arguments by reading them and saving the 'state' in a variable processed by the main function.
+ * Handles the arguments by reading them and saving the 'state' in a variable processed by the main function.
  * @param argc               : the number of arguments
  * @param argv               : array of strings, the arguments
  * @return struct settings * : settings structure that stores the code and some flags
  */
-struct settings *handle_args(const int argc, const char * const * const argv);
+struct settings *handle_args(const int argc, char **argv);
 
 #endif
