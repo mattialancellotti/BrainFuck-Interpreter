94921f7d031644a6f69837abf80cefff04b99d5b
pixelunknown <lancellottimattia@gmail.com>
2021-07-30 17:21:56 +0200

    Updated the doc

--- include/brain/args.h
+++ include/brain/args.h
@@ -1,14 +1,12 @@
 #ifndef ARGS_H
 #define ARGS_H
 
-//ARGS_OPTIONS
 #define VERSION (1)
 #define HELP    (2)
-#define INFO    (3)
-#define STATS   (4) // don't know what this is
 
 /* TODO: update this doc
  * Handles the arguments by reading them and saving the 'state' in a variable processed by the main function.
+ * @param actions            : a pointer to an int to keep track of args
  * @param argc               : the number of arguments
  * @param argv               : array of strings, the arguments
  * @return struct settings * : settings structure that stores the code and some flags
