39b9f6e978a0a96f9f7c74a2b9775ed906e31dc6
pixelunknown <lancellottimattia@gmail.com>
2021-08-16 19:29:02 +0200

    Minor changes like a forgotten header or comment

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -11,6 +12,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+/* My headers */
 #include <brain/args.h>
 #include <brain/main.h>
 
